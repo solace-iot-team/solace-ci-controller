ec68acaa77df2d85b090c6ef81856005699bf561
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-10-27 14:17:35 +0000

    dddd

--- azure/.test/_run.apply.sh
+++ azure/.test/_run.apply.sh
@@ -36,6 +36,8 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
     for i in {1..10}; do
         echo "loop counter: $i"
         sleep 5s
+        # artificial error:
+        if [ $i -eq 8 ]; then echo ">>> ERROR - 1 - $scriptName - ARTIFICIAL ERROR"; exit 1; fi
     done
     
     code=$?; if [[ $code != 0 ]]; then echo ">>> ERROR - $code - $scriptName - executing terraform"; exit 1; fi
